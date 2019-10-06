ddbde7bfa1eee992b39c35d6f56735dae617393f
Usong Cho <silvermouse4012@gmail.com>
2019-10-05 20:12:16 -0700

    props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 // import { directive } from '@babel/types';
 import Person from './Person/Person'
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Hi I'm a React App</h1>
-            <Person />
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi I'm a React App</h1>
+                <Person name="Max" age="28"/>
+                <Person name="Mark" age="26">My Hobbies: Racing</Person>
+                <Person name="Stephanie" age="19,"/>
+            </div>
+        );
+    };
 };
-
 
 export default App;
