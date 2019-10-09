fefe124b0a3f5b7d3a82561491262f542278c667
Usong Cho <silvermouse4012@gmail.com>
2019-10-09 09:03:46 -0700

    added inline and separated css file

--- src/App.js
+++ src/App.js
@@ -13,35 +13,42 @@ class App extends Component {
         ]
     };
     
-    switchNameHandler = () => {
-
-        let name = this.state.person[0].name;
-        if (name === 'Max') {
-            this.setState({
-                person: [
-                    { name: 'Dan', age: 21 },
-                    { name: 'Don', age: 12 },
-                    { name: 'Dory', age: 29 },
-                ]
-            }); 
-        } else {
-            this.setState({
-                person: [
-                    { name: 'Max', age: 21 },
-                    { name: 'Mark', age: 12 },
-                    { name: 'Mary', age: 29 },
-                ]       
-            })
-        };
+    switchNameHandler = (newName) => {
+        this.setState({
+            person: [
+                { name: newName, age: 21 },
+                { name: 'Dory', age: 12 },
+                { name: 'David', age: 29 },
+            ]
+        });
     };
 
-    render () {
+    nameChagedHandler = (event) => {
+        this.setState({
+            person: [
+                { name: 'Max', age: 21 },
+                { name: event.target.value, age: 12 },
+                { name: 'David', age: 29 },
+            ]
+        });
+    }
+
+    render() {
+
+         const style = {
+             backgroundColor: 'white',
+             font: 'inherit',
+             border: '1px solid blue',
+             padding: '8px',
+             cursor: 'pointer',
+         };
+
         return (
             <div className="App">
                 <h1>Hi I'm a React App</h1>
-                <button onClick={this.switchNameHandler}>Change Name</button>
+                <button style={style} onClick={() => this.switchNameHandler('Maximilian')}>Change Name</button>
                 <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-                <Person name={this.state.person[1].name} age={this.state.person[1].age} click={this.switchNameHandler}>My Hobbies: Racing</Person>
+                <Person name={this.state.person[1].name} age={this.state.person[1].age} click={this.switchNameHandler.bind(this, 'Maximilian')} changed={this.nameChagedHandler}>My Hobbies: Racing</Person>
                 <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
             </div>
         );
@@ -58,14 +65,6 @@ export default App;
 
 
  
-
-
-
-
-
-
-
-
 
 
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I'm {props.age} year old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     ); 
 };
 
 export default person;
+
+
+
 
 
 
